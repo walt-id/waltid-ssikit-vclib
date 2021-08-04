e017c95e618b16a4d572cfe807f66af37ca69a83
Alfred Tuffreau <alfred.tuffreau@bcdiploma.com>
2021-08-04 16:13:02 +0200

    Init Europass and JSON schema generator

---
+++ src/main/kotlin/id/walt/vclib/vclist/Europass.kt
@@ -0,0 +1,226 @@
+package id.walt.vclib.vclist
+
+
+import com.beust.klaxon.Json
+import id.walt.vclib.model.CredentialSchema
+import id.walt.vclib.model.CredentialStatus
+import id.walt.vclib.model.Proof
+import id.walt.vclib.model.VerifiableCredential
+import id.walt.vclib.registry.VerifiableCredentialMetadata
+
+data class Europass(
+    @Json(name = "@context")
+    var context: List<String> = listOf(
+        "https://www.w3.org/2018/credentials/v1"
+    ),
+    @Json(serializeNull = false) var id: String? = null, // education#higherEducation#51e42fda-cb0a-4333-b6a6-35cb147e1a88
+    @Json(serializeNull = false) var issuer: String? = null, // did:ebsi:2LGKvDMrNUPR6FhSNrXzQQ1h295zr4HwoX9UqvwAsenSKHe9
+    @Json(serializeNull = false) var issuanceDate: String? = null, // 2020-11-03T00:00:00Z
+    @Json(serializeNull = false) var validFrom: String? = null, // 2020-11-03T00:00:00Z
+    @Json(serializeNull = false) var expirationDate: String? = null,
+    @Json(serializeNull = false) var credentialSubject: CredentialSubject? = null,
+    @Json(serializeNull = false) var credentialStatus: CredentialStatus? = null,
+    @Json(serializeNull = false) var credentialSchema: CredentialSchema? = null,
+    @Json(serializeNull = false) var evidence: Evidence? = null,
+    //@Json(serializeNull = false) var proof: List<Proof>? = null
+    @Json(serializeNull = false) var proof: Proof? = null
+) : VerifiableCredential(type) {
+    companion object : VerifiableCredentialMetadata(
+        type = listOf("VerifiableCredential", "VerifiableAttestation", "Europass"),
+        template = {
+            Europass(
+                id = "education#higherEducation#51e42fda-cb0a-4333-b6a6-35cb147e1a88",
+                issuer = "did:ebsi:2LGKvDMrNUPR6FhSNrXzQQ1h295zr4HwoX9UqvwAsenSKHe9",
+                issuanceDate = "2020-11-03T00:00:00Z",
+                validFrom = "2020-11-03T00:00:00Z",
+                credentialSubject = CredentialSubject(
+                    id = "did:ebsi:22AhtW7XMssv7es4YcQTdV2MCM3c8b1VsiBfi5weHsjcCY9o",
+                    identifier = "0904008084H",
+                    givenNames = "Jane",
+                    familyName = "DOE",
+                    dateOfBirth = "1993-04-08",
+                    gradingScheme = CredentialSubject.GradingScheme(
+                        id = "https://blockchain.univ-lille.fr/ontology#GradingScheme",
+                        title = "Lower Second-Class Honours"
+                    ),
+                    learningAchievement = CredentialSubject.LearningAchievement(
+                        id = "https://blockchain.univ-lille.fr/ontology#LearningAchievment",
+                        title = "MASTERS LAW, ECONOMICS AND MANAGEMENT",
+                        description = "MARKETING AND SALES",
+                        additionalNote = listOf(
+                            "DISTRIBUTION MANAGEMENT"
+                        )
+                    ),
+                    awardingOpportunity = CredentialSubject.AwardingOpportunity(
+                        id = "https://blockchain.univ-lille.fr/ontology#AwardingOpportunity",
+                        identifier = "https://certificate-demo.bcdiploma.com/check/87ED2F2270E6C41456E94B86B9D9115B4E35BCCAD200A49B846592C14F79C86BV1Fnbllta0NZTnJkR3lDWlRmTDlSRUJEVFZISmNmYzJhUU5sZUJ5Z2FJSHpWbmZZ",
+                        awardingBody = CredentialSubject.AwardingOpportunity.AwardingBody(
+                            id = "did:ebsi:2LGKvDMrNUPR6FhSNrXzQQ1h295zr4HwoX9UqvwAsenSKHe9",
+                            eidasLegalIdentifier = "Unknown",
+                            registration = "0597065J",
+                            preferredName = "Université de Lille",
+                            homepage = "https://www.univ-lille.fr/"
+                        ),
+                        location = "FRANCE",
+                        startedAtTime = "Unknown",
+                        endedAtTime = "2020-11-03T00:00:00Z"
+                    ),
+                    learningSpecification = CredentialSubject.LearningSpecification(
+                        id = "https://blockchain.univ-lille.fr/ontology#LearningSpecification",
+                        iSCEDFCode = listOf(
+                            "7"
+                        ),
+                        eCTSCreditPoints = 120,
+                        eQFLevel = 7,
+                        nQFLevel = listOf(
+                            "7"
+                        )
+                    )
+                ),
+                credentialStatus = CredentialStatus(
+                    id = "https://essif.europa.eu/status/education#higherEducation#51e42fda-cb0a-4333-b6a6-35cb147e1a88",
+                    type = "CredentialsStatusList2020"
+                ),
+                credentialSchema = CredentialSchema(
+                    id = "https://essif.europa.eu/trusted-schemas-registry/v1/schemas/to_be_obtained_after_registration_of_the_schema",
+                    type = "JsonSchemaValidator2018"
+                )
+            )
+        }
+    );
+
+    data class Evidence(
+        @Json(serializeNull = false) var id: String? = null,
+        @Json(serializeNull = false) var type: List<String?>? = null,
+        @Json(serializeNull = false) var verifier: String? = null,
+        @Json(serializeNull = false) var evidenceDocument: List<String?>? = null,
+        @Json(serializeNull = false) var subjectPresence: String? = null,
+        @Json(serializeNull = false) var documentPresence: List<String?>? = null
+    )
+
+    data class CredentialSubject(
+        @Json(serializeNull = false) var id: String? = null, // did:ebsi:22AhtW7XMssv7es4YcQTdV2MCM3c8b1VsiBfi5weHsjcCY9o
+        @Json(serializeNull = false) var identifier: String? = null, // 0904008084H
+        @Json(serializeNull = false) var givenNames: String? = null, // Jane
+        @Json(serializeNull = false) var familyName: String? = null, // DOE
+        @Json(serializeNull = false) var dateOfBirth: String? = null, // 1993-04-08
+        @Json(serializeNull = false) var gradingScheme: GradingScheme? = null,
+        @Json(serializeNull = false) var learningAchievement: LearningAchievement? = null,
+        @Json(serializeNull = false) var awardingOpportunity: AwardingOpportunity? = null,
+        @Json(serializeNull = false) var learningSpecification: LearningSpecification? = null
+    ) {
+        data class GradingScheme(
+            var id: String, // https://blockchain.univ-lille.fr/ontology#GradingScheme
+            @Json(serializeNull = false) var title: String? = null, // Lower Second-Class Honours
+            @Json(serializeNull = false) var description: String? = null
+        )
+
+        data class LearningAchievement(
+            var id: String, // https://blockchain.univ-lille.fr/ontology#LearningAchievment
+            var title: String, // MASTERS LAW, ECONOMICS AND MANAGEMENT
+            @Json(serializeNull = false) var description: String? = null, // MARKETING AND SALES
+            @Json(serializeNull = false) var additionalNote: List<String>? = null
+        )
+
+        data class AwardingOpportunity(
+            var id: String, // https://blockchain.univ-lille.fr/ontology#AwardingOpportunity
+            var identifier: String, // https://certificate-demo.bcdiploma.com/check/87ED2F2270E6C41456E94B86B9D9115B4E35BCCAD200A49B846592C14F79C86BV1Fnbllta0NZTnJkR3lDWlRmTDlSRUJEVFZISmNmYzJhUU5sZUJ5Z2FJSHpWbmZZ
+            var awardingBody: AwardingBody,
+            @Json(serializeNull = false) var location: String? = null, // FRANCE
+            @Json(serializeNull = false) var startedAtTime: String? = null, // Unknown
+            @Json(serializeNull = false) var endedAtTime: String? = null // 2020-11-03T00:00:00Z
+        ) {
+            data class AwardingBody(
+                var id: String, // did:ebsi:2LGKvDMrNUPR6FhSNrXzQQ1h295zr4HwoX9UqvwAsenSKHe9
+                var eidasLegalIdentifier: String, // Unknown
+                var registration: String, // 0597065J
+                var preferredName: String, // Université de Lille
+                @Json(serializeNull = false) var homepage: String? = null // https://www.univ-lille.fr/
+            )
+        }
+
+        data class LearningSpecification(
+            var id: String, // https://blockchain.univ-lille.fr/ontology#LearningSpecification
+            @Json(name = "ISCEDFCode") var iSCEDFCode: List<String>,
+            @Json(name = "ECTSCreditPoints") var eCTSCreditPoints: Int? = null, // 120
+            @Json(name = "EQFLevel") var eQFLevel: Int? = null, // 7
+            @Json(name = "NQFLevel") var nQFLevel: List<String>
+        )
+    }
+}
+
+/*
+@Serializable
+class Europass(
+    @SerialName("@context") override var context: List<String>,
+    override var id: String,
+    override var type: List<String>,
+    override var issuer: String,
+    override var issuanceDate: String,
+    override var validFrom: String,
+    @field:Nullable override var expirationDate: String? = null,
+    override var credentialSubject: EuropassCredentialSubject,
+    override var credentialStatus: CredentialStatus,
+    override var credentialSchema: CredentialSchema,
+    @field:Nullable override var evidence: Evidence? = null,
+    override var proof: Proof
+) : VerifiableCredential<EuropassCredentialSubject>() {
+    override fun encode() = Json { prettyPrint = true }.encodeToString(this)
+}
+
+@Serializable
+data class EuropassCredentialSubject(
+    var id: String,
+    var identifier: String,
+    var givenNames: String,
+    var familyName: String,
+    var dateOfBirth: String,
+    var gradingScheme: GradingScheme,
+    var learningAchievement: LearningAchievement,
+    var awardingOpportunity: AwardingOpportunity,
+    var learningSpecification: LearningSpecification
+) : CredentialSubject
+
+@Serializable
+data class GradingScheme(
+    var id: String,
+    @field:Nullable var title: String? = null,
+    @field:Nullable var description: String? = null,
+)
+
+@Serializable
+data class LearningAchievement(
+    var id: String,
+    var title: String,
+    @field:Nullable var description: String? = null,
+    @field:Nullable var additionalNote: List<String>? = null
+)
+
+@Serializable
+data class AwardingOpportunity(
+    var id: String,
+    var identifier: String,
+    var awardingBody: Organisation,
+    @field:Nullable var location: String? = null,
+    @field:Nullable var startedAtTime: String? = null,
+    @field:Nullable var endedAtTime: String? = null
+)
+
+@Serializable
+data class Organisation(
+    var id: String,
+    var eidasLegalIdentifier: String,
+    var registration: String,
+    var preferredName: String,
+    @field:Nullable var homepage: String? = null
+)
+
+@Serializable
+data class LearningSpecification(
+    var id: String,
+    @SerialName("ISCEDFCode") var iscedfCode: List<String>,
+    @SerialName("ECTSCreditPoints") @field:Nullable var ectsCreditPoints: Int? = null,
+    @SerialName("EQFLevel") @field:Nullable var eqfLevel: Int? = null,
+    @SerialName("NQFLevel") var nqfLevel: List<String>
+)
+ */
