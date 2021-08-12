d74c0caefcecf84b0dfdb2bfebabc96778495225
Kevin Burgmann <kevin@walt.id>
2021-08-12 19:44:42 +0200

    Move test to StringSpec

--- src/test/kotlin/id/walt/vclib/HelpersTest.kt
+++ src/test/kotlin/id/walt/vclib/HelpersTest.kt
@@ -4,25 +4,24 @@ import com.beust.klaxon.Json
 import id.walt.vclib.Helpers.toMap
 import id.walt.vclib.model.VerifiableCredential
 import id.walt.vclib.registry.VerifiableCredentialMetadata
-import io.kotest.core.spec.style.AnnotationSpec
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
-class HelpersTest : AnnotationSpec() {
+class HelpersTest : StringSpec({
+    "test toMap()" {
+        val dummyVcMap = DummyCredential().toMap()
 
-    data class Dummy(
-        @Json(name = "@context")
-        var context: List<String> = listOf("https://www.w3.org/2018/credentials/v1"),
+        dummyVcMap["@context"] shouldBe listOf("https://www.w3.org/2018/credentials/v1")
+        dummyVcMap["type"] shouldBe listOf("VerifiableCredential", "VerifiableAttestation", "DummyCredential")
+
+        dummyVcMap.containsKey("id") shouldBe false
+    }
+}) {
+    data class DummyCredential(
+        @Json(name = "@context") var context: List<String> = listOf("https://www.w3.org/2018/credentials/v1"),
         @Json(serializeNull = false) var id: String? = null
     ) : VerifiableCredential(type) {
-        companion object : VerifiableCredentialMetadata(type = listOf("VerifiableCredential", "VerifiableAttestation", "Dummy"));
-    }
-
-    @Test
-    fun testToMap() {
-        val dummyVcAsMap = Dummy().toMap()
-        assertEquals(listOf("https://www.w3.org/2018/credentials/v1"), dummyVcAsMap["@context"])
-        assertEquals(listOf("VerifiableCredential", "VerifiableAttestation", "Dummy"), dummyVcAsMap["type"])
-        assertFalse(dummyVcAsMap.containsKey("id"))
+        companion object :
+            VerifiableCredentialMetadata(listOf("VerifiableCredential", "VerifiableAttestation", "DummyCredential"))
     }
 }
