98459a441aeb8c0d137ed8b7bff1ac2d9d76b8de
Philipp Potisk <phil@walt.id>
2022-01-27 17:30:09 +0100

    Enhanced ParticipantCredential

--- src/main/kotlin/id/walt/vclib/credentials/gaiax/ParticipantCredential.kt
+++ src/main/kotlin/id/walt/vclib/credentials/gaiax/ParticipantCredential.kt
@@ -20,12 +20,15 @@ data class ParticipantCredential(
     @Json(serializeNull = false) override var expirationDate: String? = null,
     @Json(serializeNull = false) override var credentialSubject: ParticipantCredentialSubject?,
     @Json(serializeNull = false) override var credentialSchema: CredentialSchema? = null,
+    @Json(serializeNull = false) var credentialStatus: CredentialStatus? = null,
     @Json(serializeNull = false) override var proof: Proof? = null,
 ) : AbstractVerifiableCredential<ParticipantCredential.ParticipantCredentialSubject>(type) {
     data class ParticipantCredentialSubject(
         override var id: String?,
-        var type: String,
-        var programName: String,
+        @Json(serializeNull = false) var type: String? = null,
+        @Json(serializeNull = false) var programName: String? = null,
+        @Json(serializeNull = false) val domain: String? = null,
+        @Json(serializeNull = false )val ethereumAddress: String? = null,
     ) : CredentialSubject()
 
     companion object : VerifiableCredentialMetadata(
@@ -39,8 +42,18 @@ data class ParticipantCredential(
                 credentialSubject = ParticipantCredentialSubject(
                     id = "mailto:first.last@gaia-x.eu",
                     type = "ProgramMembership",
-                    programName = "Gaia-X AISBL"
-                )
+                    programName = "Gaia-X AISBL",
+                    domain = "https://example.com",
+                    ethereumAddress = "0x4C84a36fCDb7Bc750294A7f3B5ad5CA8F74C4A52"
+                ),
+                credentialSchema = CredentialSchema(
+                    id = "https://raw.githubusercontent.com/walt-id/waltid-ssikit-vclib/master/src/test/resources/schemas/ParticipantCredential.json",
+                    type = "JsonSchemaValidator2018"
+                ),
+                credentialStatus = CredentialStatus(
+                    id = "https://gaiax.europa.eu/status/participant-credential#392ac7f6-399a-437b-a268-4691ead8f176",
+                    type = "CredentialStatusList2020"
+                ),
             )
         }
     )
