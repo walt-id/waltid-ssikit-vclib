cd12eab1d6175437e5e64cede51758be9db250e5
ahmed smiai <ahmedsmiai3@gmail.com>
2022-10-29 19:36:51 +0100

    fix obc template

--- src/main/kotlin/id/walt/vclib/credentials/OpenBadgeCredential.kt
+++ src/main/kotlin/id/walt/vclib/credentials/OpenBadgeCredential.kt
@@ -11,20 +11,19 @@ data class OpenBadgeCredential(
     @Json(name = "@context") @field:PropertyName(name = "@context") @field:Required
     var context: List<String> = listOf(
         "https://www.w3.org/2018/credentials/v1",
-        "https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/jff-vc-edu-plugfest-1-context.json"
+        "https://purl.imsglobal.org/spec/ob/v3p0/context.json"
     ),
-    @Json(serializeNull = false)
-    override var id: String?,
+    @Json(serializeNull = false) override var id: String?,
+    @Json(serializeNull = false) var name: String?,
     @field:SchemaService.PropertyName("issuer") @Json(name = "issuer") var issuerObject: Issuer,
     @Json(serializeNull = false) override var issued: String?,
     override var credentialSubject: OpenBadgeCredentialSubject?,
     @Json(serializeNull = false) override var validFrom: String? = null,
     @Json(serializeNull = false) override var issuanceDate: String? = null,
     @Json(serializeNull = false) override var expirationDate: String? = null,
-    @Json(serializeNull = false) override var credentialSchema: CredentialSchema? = CredentialSchema(
-        id = "https://ngiatlantic.info/schema/V-2022-1/OpenBadgeCredential.json",
-        type = "https://json-schema.org/draft/2019-09/schema"
-    ),
+    @Json(serializeNull = false) override var credentialSchema: CredentialSchema? = null,
+
+
     @Json(serializeNull = false) override var proof: Proof? = null
     ) : AbstractVerifiableCredential<OpenBadgeCredential.OpenBadgeCredentialSubject>(type) {
 
@@ -34,33 +33,32 @@ data class OpenBadgeCredential(
 
         template = {
             OpenBadgeCredential(
-                id = null,
+                id = "urn:uuid:a63a60be-f4af-491c-87fc-2c8fd3007a58",
+                name="JFF x vc-edu PlugFest 2 Interoperability",
                 issuerObject = Issuer(
                     "Profile",
                     "did:key:z6MkrHKzgsahxBLyNAbLQyB1pcWNYC9GmywiWPgkrvntAZcj",
-                    "Jobs for the Future (JFF)",
-                    "https://kayaelle.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png",
-                    "https://kayaelle.github.io/vc-ed/plugfest-1-2022/images/JFF_LogoLockup.png"
+                    "Jobs for the Future (JFF)"
                 ),
                 issued = null,
-                issuanceDate = "2020-03-10T04:24:12.164Z",
+                issuanceDate = "2022-11-14T00:00:00Z",
                 credentialSubject = OpenBadgeCredentialSubject(
                     type = "AchievementSubject",
-                    id = "did:jwk:1235667890",
+                    id = "did:key:123",
                     OpenBadgeCredentialSubject.Achievement (
+                        id=null,
                         type = "Achievement",
-                        name = "Our Wallet Passed JFF Plugfest #1 2022",
-                        description = "This wallet can display this Open Badge 3.0",
+                        name = "JFF x vc-edu PlugFest 2 Interoperability",
+                        description = "This credential solution supports the use of OBv3 and w3c Verifiable Credentials and is interoperable with at least two other solutions.  This was demonstrated successfully during JFF x vc-edu PlugFest 2.",
                         criteria = OpenBadgeCredentialSubject.Achievement.Criteria(
                             type = "Criteria",
-                            narrative = "The first cohort of the JFF Plugfest 1 in May/June of 2021 collaborated to push interoperability of VCs in education forward."
+                            narrative = "Solutions providers earned this badge by demonstrating interoperability between multiple providers based on the OBv3 candidate final standard, with some additional required fields. Credential issuers earning this badge successfully issued a credential into at least two wallets.  Wallet implementers earning this badge successfully displayed credentials issued by at least two different credential issuers."
                         ),
-                        image = "https://w3c-ccg.github.io/vc-ed/plugfest-1-2022/images/plugfest-1-badge-image.png",
+                        image = OpenBadgeCredentialSubject.Achievement.Image(
+                            type = "Image",
+                            id = "https://w3c-ccg.github.io/vc-ed/plugfest-2-2022/images/JFF-VC-EDU-PLUGFEST2-badge-image.png"
+                        )
                     )
-                ),
-                credentialSchema = CredentialSchema(
-                    id = "https://ngiatlantic.info/schema/V-2022-1/OpenBadgeCredential.json",
-                    type = "https://json-schema.org/draft/2019-09/schema"
                 )
             )
         }
@@ -69,9 +67,7 @@ data class OpenBadgeCredential(
     data class Issuer(
         var type: String,
         var id: String,
-        var name: String,
-        @Json(serializeNull = false) var url: String? = null,
-        @Json(serializeNull = false) var image: String? = null
+        var name: String
     )
 
     data class OpenBadgeCredentialSubject(
@@ -80,15 +76,22 @@ data class OpenBadgeCredential(
         var achievement: Achievement
     ) : CredentialSubject() {
         data class Achievement(
+            var id: String?,
             var type: String,
             var name: String,
             @Json(serializeNull = false) var description: String? = null,
             var criteria: Criteria,
-            @Json(serializeNull = false) var image: String? = null,
+            var image: Image
+
         ){
             data class Criteria(
                 var type: String,
                 @Json(serializeNull = false) var narrative: String? = null,
+            )
+            data class Image(
+                var type: String,
+                @Json(serializeNull = false) var id: String? = null,
+
             )
         }
     }
