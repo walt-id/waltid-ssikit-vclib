a9755ba893c47862f7040f37ba81f7ced99297a1
Severin Stampler <severin@walt.id>
2022-11-09 17:28:28 +0100

    undo VerifiableAttestation changes, which are not required for wallet conformance.

--- src/main/kotlin/id/walt/vclib/credentials/VerifiableAttestation.kt
+++ src/main/kotlin/id/walt/vclib/credentials/VerifiableAttestation.kt
@@ -5,7 +5,6 @@ import id.walt.vclib.model.*
 import id.walt.vclib.registry.VerifiableCredentialMetadata
 import id.walt.vclib.schema.SchemaService.PropertyName
 import id.walt.vclib.schema.SchemaService.Required
-import java.util.UUID
 
 data class VerifiableAttestation(
     @Json(name = "@context") @field:PropertyName(name = "@context") @field:Required
@@ -29,9 +28,9 @@ data class VerifiableAttestation(
         var id: String, // https://essif.europa.eu/tsr-va/evidence/f2aeec97-fc0d-42bf-8ca7-0548192d5678
         var type: List<String>,
         var verifier: String, // did:ebsi:2962fb784df61baa267c8132497539f8c674b37c1244a7a
-        var evidenceDocument: List<String>, // Passport
+        var evidenceDocument: String, // Passport
         var subjectPresence: String, // Physical
-        var documentPresence: List<String> // Physical
+        var documentPresence: String // Physical
     )
 
     companion object : VerifiableCredentialMetadata(
@@ -41,30 +40,30 @@ data class VerifiableAttestation(
                 context = listOf(
                     "https://www.w3.org/2018/credentials/v1",
                 ),
-                id = "urn:uuid:${UUID.randomUUID()}",
+                id = "education#higherEducation#3fea53a4-0432-4910-ac9c-69ah8da3c37f",
                 issuer = "did:ebsi:2757945549477fc571663bee12042873fe555b674bd294a3",
                 issued = "2019-06-22T14:11:44Z",
                 expirationDate = "2022-06-22T14:11:44Z",
                 validFrom = "2019-06-22T14:11:44Z",
                 credentialSubject = VerifiableAttestationSubject(
-                    id = "did:ebsi:1234"
+                    id = "id123"
                 ),
                 credentialStatus = CredentialStatus(
-                    id = "https://essif.europa.eu/status/identity",
+                    id = "https://essif.europa.eu/status/identity#verifiableID#1dee355d-0432-4910-ac9c-70d89e8d674e",
                     type = "CredentialStatusList2020"
                 ),
                 credentialSchema = CredentialSchema(
                     id = "https://raw.githubusercontent.com/walt-id/waltid-ssikit-vclib/master/src/test/resources/schemas/VerifiableAttestation.json",
-                    type = "FullJsonSchemaValidator2021"
+                    type = "JsonSchemaValidator2018"
                 ),
                 evidence = listOf(
                     Evidence(
                         id = "https://essif.europa.eu/tsr-va/evidence/f2aeec97-fc0d-42bf-8ca7-0548192d5678",
                         type = listOf("DocumentVerification"),
                         verifier = "did:ebsi:2962fb784df61baa267c8132497539f8c674b37c1244a7a",
-                        evidenceDocument = listOf("Passport"),
+                        evidenceDocument = "Passport",
                         subjectPresence = "Physical",
-                        documentPresence = listOf("Physical")
+                        documentPresence = "Physical"
                     )
                 )
             )
